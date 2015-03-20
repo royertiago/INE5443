23855904b8ee264040cf5f7919d2b4010d297a50
Tiago Royer <royertiago@gmail.com>
2015-03-20 00:03:06 -0300

    Add DataEntry interface.

---
+++ pr/data_entry.h
@@ -0,0 +1,36 @@
+#ifndef DATA_ENTRY_H
+#define DATA_ENTRY_H
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+class DataEntry {
+    std::vector< double > attributes;
+    std::vector< std::string > categories;
+
+public:
+
+    DataEntry( std::vector< double > &&, std::vector< std::string >&& );
+
+    const double * begin() const;
+    const double * end() const;
+
+    double attribute( std::size_t index ) const;
+    const std::string& category( std::size_t index ) const;
+
+    std::size_t attribute_count() const;
+    std::size_t category_count() const;
+
+    /* Parses a dummy entry from the specified file.
+     * The entry will have the specified number of attributes,
+     * but an empty category vector.
+     *
+     * If EOF is reached, a DataEntry
+     * with less than size attributes is returned.
+     *
+     * The format is assumed to be comma-separated. */
+    static DataEntry parseEntry( FILE * file, std::size_t size );
+};
+
+#endif // DATA_ENTRY_H
