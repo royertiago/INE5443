e8e92850908f94c24912b54b6327bc71b68a4393
Tiago Royer <royertiago@gmail.com>
2015-03-20 13:47:35 -0300

    Improve interface in pr/data_set.h and implement it.

---
+++ pr/data_set.cpp
@@ -0,0 +1,92 @@
+#include "pr/data_set.h"
+
+DataSet::DataSet(
+    std::vector< DataEntry >&& entries,
+    std::vector< std::string >&& attribute_names,
+    std::vector< std::string >&& category_names
+) :
+    entries(entries),
+    attribute_names(attribute_names),
+    category_names(category_names)
+{}
+
+DataSed DataSet::parse( std::FILE * source ) {
+    std::size_t field_count = 0;
+    char c;
+
+    while( std::fscanf( source, "%c", &c ) != EOF ) {
+        if( c == '#' ) {
+            std::fscanf( source, "%*[^\n]\n" );
+            continue;
+        }
+        if( c == 'n' ) {
+            std::fscanf( source, " %zd \n", &field_count );
+            break;
+        }
+        throw "Unknown directive.";
+    }
+    if( field_count == 0 )
+        throw "No fields in dataset.";
+
+    std::vector< std::string > attribute_names;
+    std::vector< std::string > category_names;
+    std::string format;
+
+    while( field_count-- ) {
+        std::fscanf( source, "%c ", &c );
+        format += c;
+        std::string name;
+        while( (c = std::fgetc(source)) != '\n' )
+            name += c;
+        if( c == 'a' )
+            attribute_names.push_back(name);
+        else if( c == 'c' )
+            category_names.push_back(name);
+        else
+            throw "Unknown field type.";
+    }
+
+    std::vector< DataEntry > entries;
+    DataEntry entry;
+
+    while( entry = DataEntry::parse( source, format.c_str() ) ) {
+        if( entry.attribute_count() < attribute_names.size() ||
+            entry.category_cout() < category_names.size() )
+            break;
+        entries.push_back( entry );
+    }
+
+    return DataSet(
+        std::move(entries),
+        std::move(attribute_names),
+        std::move(category_names)
+    );
+}
+
+const DataEntry * DataSet::begin() const {
+    return &*entries.begin();
+}
+
+const DataEntry * DataSet::end() const {
+    return &*entries.end();
+}
+
+std::size_t DataSet::size() const {
+    return entres.size();
+}
+
+std::size_t DataSet::attribute_count() const {
+    return attribute_names.size();
+}
+
+std::size_t DataSet::category_count() const {
+    return category_name.count();
+}
+
+std::string DataSet::attribute_name( std::size_t index ) {
+    return attribute_names[index];
+}
+
+std::string DataSet::category_name( std::size_t index ) {
+    return category_names[index];
+}
--- pr/data_set.h
+++ pr/data_set.h
@@ -1,23 +1,32 @@
 #ifndef DATA_SET_H
 #define DATA_SET_H
 
-#include <stdio.h>
+#include <cstdio>
+#include <string>
 #include <vector>
 #include "pr/data_entry.h"
 
 class DataSet {
     std::vector< DataEntry > entries;
+    std::vector< std::string > attribute_names;
+    std::vector< std::string > category_names;
 
 public:
-    explicit DataSet( const std::vector< DataEntry >& entries );
-    explicit DataSet( std::vector< DataEntry >&& entries );
-    static DataSet parse( FILE * source );
+    DataSet(
+        std::vector< DataEntry >&& entries,
+        std::vector< std::string >&& attribute_names,
+        std::vector< std::string >&& category_names
+    );
+
+    static DataSet parse( std::FILE * source );
 
     const DataEntry * begin() const;
     const DataEntry * end() const;
     std::size_t size() const;
     std::size_t attribute_count() const;
     std::size_t category_count() const;
+    std::string attribute_name( std::size_t index ) const;
+    std::string category_name( std::size_t index ) const;
 };
 
 #endif // DATA_SET_H
