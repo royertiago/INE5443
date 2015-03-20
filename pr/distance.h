1d2c9e0689d885762f44f888e54970f9afa81e34
Tiago Royer <royertiago@gmail.com>
2015-03-20 00:04:03 -0300

    Update DistanceCalculator interface.

--- pr/distance.h
+++ pr/distance.h
@@ -1,21 +1,40 @@
 #ifndef DISTANCE_H
 #define DISTANCE_H
 
+class DataSet;
+class DataEntry;
+
 struct DistanceCalculator {
-    virtual double compute( const double * begin1, const double * begin2 ) = 0;
+    virtual double operator()( const DataEntry&, const DataEntry& ) const = 0;
+    virtual void normalize( const DataEntry& ) = 0;
 };
 
-class EuclideanDistance : public DistanceCalculator {
+
+class NormalizingDistanceCalculator : public DistanceCalculator {
+private:
+    double tolerance;
+protected:
+    std::vector< double > minimum_value;
+    std::vector< double > multiplicative_factor;
+public:
+    NormalizingDistanceCalculator( double tolerance );
+    virtual double operator()( const DataEntry&, const DataEntry& ) const = 0;
+    virtual void normalize( const DataEntry& ) override;
+};
+
+
+class EuclideanDistance : public NormalizingDistanceCalculator {
     unsigned size;
 public:
-    EuclideanDistance( unsigned size );
+    EuclideanDistance( unsigned size, double normalizing_tolarance );
     virtual double compute( const double * begin1, const double * begin2 ) override;
 };
 
-struct ManhattanDistance : public DistanceCalculator {
+
+struct ManhattanDistance : public NormalizingDistanceCalculator {
 public:
     unsigned size;
-    ManhattanDistance( unsigned size );
+    ManhattanDistance( unsigned size, double normalizing_tolerance );
     virtual double compute( const double * begin1, const double * begin2 ) override;
 };
 
