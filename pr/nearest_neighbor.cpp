611aa745bc603aa57224781ca70832268e1fcdd1
Tiago Royer <royertiago@gmail.com>
2015-03-21 16:21:18 -0300

    Shrink interface in pr/nearest_neighbor.h and implement it.

---
+++ pr/nearest_neighbor.cpp
@@ -0,0 +1,139 @@
+#include "nearest_neighbor.h"
+#include "pr/data_entry.h"
+#include "pr/data_set.h"
+#include "pr/distance.h"
+
+NearestNeighbor::NearestNeighbor(
+    const DataSet & dataset,
+    DistanceCalculator& distance,
+    std::size_t neighbors
+) :
+    dataset( dataset ),
+    distance( distance ),
+    n( neighbors )
+{
+    distance.normalize(dataset);
+}
+
+std::vector< std::string > classify( const DataEntry & target ) {
+    std::vector< std::pair<double, const DataEntry *> > nearest;
+    for( const DataEntry & entry : dataset )
+        nearest.emplace_back( distance( entry, target ), &entry );
+
+    std::sort( nearest.begin(), nearest.end() );
+
+    std::vector< std::map<std::string, unsigned> > votes( dataset.category_count() );
+    /* votes[i] represents the votes of the nearer neighbors
+     * for the ith category that the target entry will be classified.
+     * Thus, for instance, votes[0]["Iris-versicolor"] == 4
+     * means that there are four votes for the first category
+     * to be "Iris-setosa".
+     *
+     * We begin by processing the first `this->neighbors`
+     * mandatory votes. */
+
+    auto it = nighbors.begin();
+    for( unsigned i = 0; i < neighbors; ++i, ++it ) {
+        if( it == neighbors.end() )
+            throw "Too few entries in dataset to categorize target entry.";
+        for( unsigned j = 0; j < it->category_count(); j++ )
+            count[j][it->category(j)]++;
+    }
+
+    std::vector< std::string > categories( dataset.category_count() );
+    std::vector< std::string > max_votes( dataset.category_count() );
+    /* categories[i] is the category with the highest number of votes
+     * for the ith category type,
+     * or "" in the event of a draw.
+     *
+     * First, we will process the current votes.
+     * If there are draws, we will get more votes
+     * from the nearest neighbors after the first `this->neighbors`. */
+
+    for( unsigned i = 0; i < neighbors; ++i ) {
+        for( auto pair: votes[i] ) {
+            if( pair.second > max_votes[i] ) {
+                categories[i] = pair.first;
+                max_votes[i] = pair.second;
+            }
+            else if( pair.second == max_votes[i] ) // there is a draw
+                categories[i] = "";
+        }
+    }
+
+    /* Now, the tricky part: resolving draws.
+     *
+     * We will do this by considering the next-nearest neighbor;
+     * if the draw persists
+     * (for instance, there can be two votes for A,
+     * one vote for B and two for C, and the next vote is for B),
+     * we analyse yet another neighbor, and so on.
+     * To guarantee termination,
+     * once a tie is broken,
+     * we will not consider votes for that category.
+     * That is, categories are independent.
+     *
+     * Claim:
+     * If there are C different categories for a single index
+     * and we are considering N neighbors,
+     * in the worst-case scenario we the need to investigate
+     *  (C-2) * floor(N/2) + 1
+     * neighbors in total to solve a tie with the first N entries.
+     *
+     * Proof.
+     * If we reach a draw with V votes and T tied categories,
+     * whenever the next investigated entry falls in any of these T categories,
+     * the tie is broken.
+     * The other C - T categories must have at most V votes
+     * (since V is the maximum number of votes),
+     * so we will find at most V entries in each of these categories.
+     * before breaking the tie.
+     * Thus, after analyzing at most
+     *  (C - T) * V + 1
+     * entries, we resolve the draw.
+     * Now, T is at least two (otherwise its not a tie).
+     * And, since V is determined in the first N entries,
+     * 2*V is at most N, which gives the maximum value of
+     * floor(N/2) for V.
+     * Combining both estimates gives the maximum value
+     *  (C-2) * floor(N/2) + 1.
+     * Now observe that, if N is even, this maximum is reached.
+     * If in the first N neighbors we have exactly N/2 votes
+     * for two different categories,
+     * and all votes are "wasted" in these two categories,
+     * each of the C - 2 remaining categories have no votes.
+     * So, we can have (C-2)*V more entries that do not untie
+     * this category. As V = N/2 = floor(N/2),
+     * the maximum is reached.
+     * For odd N, one vote goes to any of the C-2 other categories,
+     * so the maximum is (C-2)*floor(N/2).
+     */
+    bool draws = true;
+    while( draws ) {
+        draws = false;
+        /* The following two variables encapsulate
+         * the exception throwing
+         * whenever there is no more neighbors to consider. */
+        bool already_incremented = false;
+        auto category_of = [&](unsigned index) {
+            if( !already_incremented ) {
+                if( it == neighbors.end() )
+                    throw "Too few entries in dataset to resolve the tie.";
+                ++it;
+                already_incremented = true;
+            }
+            return it->category(index);
+        };
+        for( unsigned i = 0; i < neighbors; ++i ) {
+            if( categories[i] == "" ) {
+                votes[i][category_of(i)]++;
+                if( votes[i][category_of(i)] > max_votes[i] )
+                    categories[i] = category_of(i);
+                else
+                    draws = true;
+            }
+        }
+    }
+
+    return categories;
+}
--- pr/nearest_neighbor.h
+++ pr/nearest_neighbor.h
@@ -3,18 +3,20 @@
 
 #include <string>
 #include <vector>
-#include "pr/data_entry.h"
-#include "pr/data_set.h"
+
+class DistanceCalculator;
 
 class NearestNeighbor {
+    const DataSet & dataset;
     DistanceCalculator& distance;
     std::size_t _n; // Nearest Neighbor algorithm parameter
-    const DataSet & data_set;
 
 public:
-    NearestNeighbor( const DataSet &, std::unique_ptr<DistanceCalculator>&& );
-    std::size_t n() const;
-    void n( std::size_t );
+    NearestNeighbor(
+        const DataSet & dataset,
+        DistanceCalculator& distance,
+        std::size_t neighbors
+    );
 
     std::vector< std::string > classify( const DataEntry & );
 };
