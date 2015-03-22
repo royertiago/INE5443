5d86d928726c9a4824139d5f52f64edee11fab73
Tiago Royer <royertiago@gmail.com>
2015-03-22 17:02:17 -0300

    Add trivial tests to NearestNeighbor.

---
+++ test/nearest_neighbor.test.cpp
@@ -0,0 +1,54 @@
+#include "pr/nearest_neighbor.h"
+#include <catch.hpp>
+
+#include "pr/data_set.h"
+#include "pr/data_entry.h"
+#include "pr/distance.h"
+
+DataSet xy_data(
+    std::vector<std::string>{"X pos", "Y pos"},
+    std::vector<std::string>{"Type"},
+    std::vector<DataEntry>{
+        DataEntry({3, 4},{"A"}),
+        DataEntry({6, 0},{"B"}),
+        DataEntry({8, 0},{"A"}),
+
+        // Neutralize normalization
+        DataEntry({-10,-10},{"C"}),
+        DataEntry({10,10},{"C"})
+    }
+);
+
+std::vector<std::string> category_A = {"A"};
+std::vector<std::string> category_B = {"B"};
+std::vector<std::string> category_C = {"C"};
+
+TEST_CASE( "Euclidean Nearest Neighbor, NN=1, one category", "[nn][trivial]" ) {
+    EuclideanDistance distance(0);
+    NearestNeighbor nn(xy_data, distance, 1);
+
+    CHECK( nn.classify(DataEntry({0,0},{})) == category_A );
+    CHECK( nn.classify(DataEntry({3,0},{})) == category_B );
+    CHECK( nn.classify(DataEntry({0,4},{})) == category_A );
+    CHECK( nn.classify(DataEntry({0,7.0001},{})) == category_A );
+    CHECK( nn.classify(DataEntry({0,6.9999},{})) == category_A );
+    CHECK( nn.classify(DataEntry({3,10},{})) == category_A );
+    CHECK( nn.classify(DataEntry({5,5},{})) == category_A );
+    CHECK( nn.classify(DataEntry({8,8},{})) == category_C );
+    CHECK( nn.classify(DataEntry({-5,-5},{})) == category_C );
+}
+
+TEST_CASE( "Manhattan Nearest Neighbor, NN=1, one category", "[nn][trivial]" ) {
+    ManhattanDistance distance(0);
+    NearestNeighbor nn(xy_data, distance, 1);
+
+    CHECK( nn.classify(DataEntry({0,0},{})) == category_B );
+    CHECK( nn.classify(DataEntry({3,0},{})) == category_B );
+    CHECK( nn.classify(DataEntry({0,4},{})) == category_A );
+    CHECK( nn.classify(DataEntry({0,7.0001},{})) == category_A );
+    CHECK( nn.classify(DataEntry({0,6.9999},{})) == category_A );
+    CHECK( nn.classify(DataEntry({3,10},{})) == category_A );
+    CHECK( nn.classify(DataEntry({5,5},{})) == category_A );
+    CHECK( nn.classify(DataEntry({8,8},{})) == category_C );
+    CHECK( nn.classify(DataEntry({-5,-5},{})) == category_C );
+}
