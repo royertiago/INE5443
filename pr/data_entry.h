57b0ba9298af3fea2d5ec2a1275156ce01dcb137
Tiago Royer <royertiago@gmail.com>
2015-03-22 14:29:33 -0300

    data_entry.h: Improve documentation.

--- pr/data_entry.h
+++ pr/data_entry.h
@@ -7,36 +7,72 @@
 #include <vector>
 #include <initializer_list>
 
+/* class DataEntry
+ *
+ * This class represents a line in a dataset.
+ * It contains both attributes and categories.
+ *
+ * Order of attributes is preserved,
+ * as well as the order of categories;
+ * however, no information about the order between attributes or categories
+ * is kept in the object.
+ */
 class DataEntry {
     std::vector< double > attributes;
     std::vector< std::string > categories;
-
 public:
-    DataEntry() = default;
 
+    /* Constructs a new DataEntry from the specified vectors.
+     * The first vector is the attribute vector,
+     * the second vector is the category vector.
+     *
+     * Note that there is only one constructor taking vectors,
+     * and they must be std::move'd to the object. */
     DataEntry( std::vector< double > &&, std::vector< std::string >&& );
+
+    /* Construcs a new DataEntry from the specified initializer lists.
+     * This function is meant to ease unit testing.
+     *
+     * Note the second initializer list is a list of char pointers,
+     * not a list of strings. */
     DataEntry( std::initializer_list<double>, std::initializer_list<const char*> );
 
+    /* Equivalent to DataEntry({},{}). */
+    DataEntry() = default;
+
+    /* Return the attribute or category in the chosen index,
+     * respectively.
+     *
+     * No checking is done to assure that there is an attribute
+     * or category in the specified index. */
     double attribute( std::size_t index ) const;
     const std::string& category( std::size_t index ) const;
 
+    /* Return the number of attributes or categories
+     * of this DataEntry, respectively. */
     std::size_t attribute_count() const;
     std::size_t category_count() const;
 
-    /* Parses a dummy entry from the specified file.
-     * The entry will have the specified number of attributes,
-     * but an empty category vector.
+    /* Parses an entry according to the specified format.
      *
+     * "format" is a string consisting entirely of 'a' and 'c'.
+     * 'a' means to read an attribute;
+     * 'c' means to read a category.
      * If EOF is reached, a DataEntry
-     * with less than size attributes is returned.
+     * with less attributes or categories is returned.
      *
-     * The format is assumed to be comma-separated. */
-    static DataEntry parse( std::FILE * file, std::size_t size );
-
-    /* Parses an entry according to hte specified format.
-     * If EOF is reached, a DataEntry
-     * with less attributes of categories is returned. */
+     * Note that order of characters in 'format' matters.
+     *
+     * A trailing newline character after the last attribute or category
+     * will be discarded. */
     static DataEntry parse( std::FILE * file, const char * format );
+
+    /* Parses an entry with 'size' attributes
+     * and no category.
+     *
+     * Equivalent to call parse( FILE*, const char * format )
+     * with a string of 'size' characters 'a' in 'format'. */
+    static DataEntry parse( std::FILE * file, std::size_t size );
 
     /* Compare two DataEntries for equality.
      * The error tolerance for category_count is
