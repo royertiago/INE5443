b8a8efeaf0b273b68b22fe370cb9d55e56820da0
Tiago Royer <royertiago@gmail.com>
2015-04-08 16:12:29 -0300

    pixel_chooser.cpp: Add visual feedback to the user.

--- mahalanobis/pixel_chooser.cpp
+++ mahalanobis/pixel_chooser.cpp
@@ -2,9 +2,11 @@
 #include <cstring>
 #include <vector>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
 
 std::vector<std::vector<bool>> chosen;
 cv::Mat img;
+const char * window = "Choose the points";
 
 int main( int argc, char ** argv ) {
     if( argc != 2 || strcmp(argv[1], "--help") == 0 ) {
@@ -20,7 +22,7 @@ int main( int argc, char ** argv ) {
         return 0;
     }
 
-    img = cv::imread( argv[1] );
+    img = cv::imread( argv[1], CV_LOAD_IMAGE_COLOR );
     if( img.empty() ) {
         std::fprintf( stderr, "Error opening image.\n" );
         return 1;
@@ -31,19 +33,26 @@ int main( int argc, char ** argv ) {
         std::vector<bool>(img.rows, false)
     );
 
-    cv::namedWindow( "Choose the points", cv::WINDOW_AUTOSIZE );
-    cv::setMouseCallback( "Choose the points",
-        []( int event, int x, int y, int flags, void * /*user_data*/ ) {
+    cv::namedWindow( window, cv::WINDOW_AUTOSIZE );
+    cv::setMouseCallback( window,
+        []( int event, int x, int y, int /*flags*/, void * /*user_data*/ ) {
             if( event == cv::EVENT_LBUTTONDOWN ) {
                 if( !chosen[x][y] )
                     std::printf( "%i,%i\n", x, y );
                 chosen[x][y] = true;
+                cv::circle( img, cv::Point(x, y), 0, cv::Scalar(0, 0, 0), 6);
+                cv::imshow( window, img );
             }
         },
         nullptr
     );
-    cv::imshow( "Choose the points", img );
-    cv::waitKey( 0 );
+    cv::imshow( window, img );
+
+    // No idea why I cannot use 'while( (char) cv::waitKey() != 0 )'...
+    while( true ) {
+        char c = cv::waitKey();
+        if( c != 0 ) break;
+    }
 
     return 0;
 }
