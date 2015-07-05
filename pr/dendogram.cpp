dd7d13acfcc2adb416a91e7213012145101575fa
Tiago Royer <royertiago@gmail.com>
2015-07-05 16:13:14 -0300

    pr/dendogram.h: Add function classify_dendogram.

--- pr/dendogram.cpp
+++ pr/dendogram.cpp
@@ -1,8 +1,7 @@
 #include <limits>
 #include <list>
+#include <sstream>
 #include "pr/dendogram.h"
-#include "pr/dendogram_node.h"
-#include "pr/data_set.h"
 #include "pr/p_norm.h"
 
 std::unique_ptr<DendogramNode> generate_dendogram(
@@ -76,4 +75,115 @@ double MeanLinkage( const DendogramNode & a, const DendogramNode & b ) {
             d += dist(entry1, entry2);
         }
     return d / count;
+}
+
+DataSet classify_dendogram(
+    const DendogramNode & root,
+    int minClass,
+    int maxClass,
+    const DataSet & base
+)
+{
+    if( maxClass > root.size() )
+        throw "maxClass is greater than the number of leaves in the dendogram.";
+    if( minClass > maxClass )
+        throw "minClass > maxClass.";
+
+    std::vector< const DendogramNode * > classes;
+    classes.reserve( maxClass );
+    classes.push_back( &root );
+
+    /* We will iterate through every possible splitting of the dendogram
+     * in k classes.
+     * The list 'classes' will hold the list of nodes
+     * that are at the top of its classes
+     * when we divide the dendogram in $k$ different classes.
+     *
+     * Note that positional information about classes is irrelevant,
+     * so we may use a std::vector instead of a std::list.
+     */
+
+    /* Splits the vector 'classes' in the node with highest linkage distance.
+     * Returns the second-highest linkage distance in that vector.
+     */
+    auto split_classes = [&](){
+        double highest_linkage_distance = 0.0;
+        double second_highest_linkage_distance = 0.0;
+        unsigned index_of_highest = 0;
+
+        // First, find the correct index
+        for( int i = 0; i < classes.size(); i++ )
+            if( classes[i]->linkage_distance() > highest_linkage_distance ) {
+                second_highest_linkage_distance = highest_linkage_distance;
+                highest_linkage_distance = classes[i]->linkage_distance();
+                index_of_highest = i;
+            }
+
+        // Now, split!
+        const DendogramNode * old_class = classes[index_of_highest];
+        classes[index_of_highest] = &old_class->left();
+        classes.push_back( &old_class->right() );
+
+        if( old_class->left().linkage_distance() > second_highest_linkage_distance )
+            second_highest_linkage_distance = old_class->left().linkage_distance();
+        if( old_class->right().linkage_distance() > second_highest_linkage_distance )
+            second_highest_linkage_distance = old_class->right().linkage_distance();
+        return second_highest_linkage_distance;
+    };
+
+    // Highest linkage distante when we last divided the 'classes' vector.
+    double current_linkage_distance = root.linkage_distance();
+    // Previous value of current_linkage_distance
+    double previous_linkage_distance = root.linkage_distance();
+
+    // First, we will add minClass classes to 'classes'.
+    for( int i = 1; i < minClass; i++ ) {
+        previous_linkage_distance = current_linkage_distance;
+        current_linkage_distance = split_classes();
+    }
+
+    /* Now, we will iterate for every k in the range [minClass, maxClass]
+     * and choose the one that maximizes the linkage delta.
+     *
+     * linkage delta is the difference between
+     * the linkage distance of the class we just split
+     * and the linkage distance of the class we will split next.
+     */
+    std::vector< const DendogramNode * > best_split = classes;
+    double best_linkage_delta = previous_linkage_distance - current_linkage_distance;
+
+    for( int k = minClass; k < maxClass; k++ ) {
+        previous_linkage_distance = current_linkage_distance;
+        current_linkage_distance = split_classes();
+        if( previous_linkage_distance - current_linkage_distance > best_linkage_delta ) {
+            best_linkage_delta = previous_linkage_distance - current_linkage_distance;
+            best_split = classes;
+        }
+    }
+
+    // Finally, construct and return the dataset.
+    std::vector< std::string > attribute_names;
+    for( int i = 0; i < base.attribute_count(); i++ )
+        attribute_names.push_back( base.attribute_name(i) );
+    std::vector< std::string > category_names{ "Class" };
+    std::vector< DataEntry > entries;
+
+    int class_number = 1;
+    std::stringstream class_name;
+    for( const DendogramNode * ptr : best_split ) {
+        class_name.str( "" );
+        class_name << "C" << class_number++;
+        for( const auto& entry : *ptr )
+            entries.emplace_back(
+                std::vector<double>( entry.attributes() ),
+                std::vector<std::string>{ class_name.str() },
+                entry.name()
+            );
+    }
+
+    return DataSet(
+        std::move( attribute_names ),
+        std::move( category_names ),
+        std::move( entries )
+    );
 }
--- pr/dendogram.h
+++ pr/dendogram.h
@@ -4,8 +4,8 @@
 // Utilities to build a dendogram from a DataSet.
 
 #include <memory>
-class DendogramNode;
-class DataSet;
+#include "pr/data_set.h"
+#include "pr/dendogram_node.h"
 
 /* Type that represents a function that takes a pair of DendogramNodes
  * and return their relative distance.
@@ -36,5 +36,30 @@ double FullLinkage( const DendogramNode &, const DendogramNode & );
 
 // Mean linkage: uses the average distance instead of the smallest.
 double MeanLinkage( const DendogramNode &, const DendogramNode & );
+
+/* Given two parameters minClass and maxClass,
+ * this function will find the "optimum cut" k (minClass <= k <= maxClass)
+ * and divide the dendogram in k different classes.
+ * The returned DataSet is the same dataset referred by the DendogramNode,
+ * but each DataEntry will have a single category.
+ * (with name "C1", "C2", ..., or "Ck")
+ *
+ * (We will extract attribute names from the dataset 'base'.)
+ *
+ * By "optimum cut" we mean a $k$ that maximizes the difference
+ * between the maximum distance linkage with $k$ and $k+1$ classes.
+ * In terms of the dendogram visualization, we will cut the classes
+ * in a place that maximizes the horizontal distance
+ * between the two closest nodes.
+ *
+ * (Note that we will never put everything in a single class
+ * unless the maxClass parameter forces us to.)
+ */
+DataSet classify_dendogram(
+    const DendogramNode &,
+    int minClass,
+    int maxClass,
+    const DataSet& base
+);
 
 #endif // DENDOGRAM_H
