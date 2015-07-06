5317cce4612cb87526d9316d1a62956819ddab04
Tiago Royer <royertiago@gmail.com>
2015-07-05 22:21:52 -0300

    pr/dendogram.h: Improve notation to declare LinkageDistanceFunctions.

--- pr/dendogram.h
+++ pr/dendogram.h
@@ -23,19 +23,16 @@ std::unique_ptr<DendogramNode> generate_dendogram(
     LinkageDistanceFunction
 );
 
-/* Built-in linkage distance calculators.
- */
-
 /* Simple linkage: the distance is the smallest distance
  * between an entry in the first dataset and an entry in the second dataset.
  */
-double SimpleLinkage( const DendogramNode &, const DendogramNode & );
+LinkageDistanceFunction SimpleLinkage;
 
 // Full linkage: uses the greatest distance instead of the smallest.
-double FullLinkage( const DendogramNode &, const DendogramNode & );
+LinkageDistanceFunction FullLinkage;
 
 // Mean linkage: uses the average distance instead of the smallest.
-double MeanLinkage( const DendogramNode &, const DendogramNode & );
+LinkageDistanceFunction MeanLinkage;
 
 /* Given two parameters minClass and maxClass,
  * this function will find the "optimum cut" k (minClass <= k <= maxClass)
